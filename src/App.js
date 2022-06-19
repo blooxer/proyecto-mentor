6db9a517e53a129ce8b9d8ed8a60fe457a7b1769
blooxer <gonzaloramos114@gmail.com>
2022-06-19 12:36:32 -0300

    codigo-fuente

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import NAv from './Componentes/Nav';
+import Main from './Componentes/Main'
+
+function App() {
+  return (
+    <div className="App">
+    <NAv/>
+    <Main />
+    </div>
+  );
+}
+
+export default App;
